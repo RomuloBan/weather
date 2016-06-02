184ccc65ab18f95280bbdfa9729eca63f17ade85
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-06-02 23:59:56 +0200

    add connect and mapDispatchToProps functions

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
 	constructor (props) {
 		super(props);
@@ -19,11 +22,15 @@ export default class SearchBar extends Component {
 
 	onFormSubmit(event) {
 		event.preventDefault();
+
+		// We need to go and fetch weather data
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: ''});
 	}
 
 	render() {
 		return (
-			<form onSubmit={this.onFormSubmit} className='input-group'>
+			<form onSubmit={this.onFormSubmit.bind(this)} className='input-group'>
 				<input
 					ref={ref => this.input = ref}
 					placeholder='Get a five-day forecast in your favorite cities'
@@ -38,3 +45,10 @@ export default class SearchBar extends Component {
 		);
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
+
