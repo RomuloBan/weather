582c46ec3e3d57bcf8bcae7d3806841e3a6278ec
Romulo Banares <romulo.banares.contractor@bbva.com>
2016-06-29 00:27:48 +0200

    add symbols to fields values

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -11,9 +11,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 
 				</thead>
@@ -27,19 +27,19 @@ class WeatherList extends Component {
 
 	renderWeather(cityData) {
 		const temps = cityData.list.map(weather => weather.main.temp);
-		const humidities = cityData.list.map(weather => weather.main.humidity);
 		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
 
 		return (<tr key={cityData.city.id}>
 					<td>{cityData.city.name}</td>
 					<td>
-						<Chart data={temps} color='orange' />
+						<Chart data={temps} color='orange' units='K' />
 					</td>
 					<td>
-						<Chart data={humidities} color='blue' />
+						<Chart data={pressures} color='blue' units='hPa' />
 					</td>
 					<td>
-						<Chart data={pressures} color='green' />
+						<Chart data={humidities} color='green' units='%' />
 					</td>			
 				</tr>);
 
